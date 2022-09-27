c80f4c075bd3f5c58606f404db1b238bd1ec0cc9
MrCrayfish <mrcrayfish@hotmail.com>
2022-09-27 11:00:51 +0930

    📦 Added button to pause menu

--- src/main/java/com/mrcrayfish/catalogue/client/ClientHandler.java
+++ src/main/java/com/mrcrayfish/catalogue/client/ClientHandler.java
@@ -5,6 +5,7 @@ import com.mrcrayfish.catalogue.client.screen.widget.CatalogueIconButton;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 
@@ -22,6 +23,16 @@ public class ClientHandler implements ClientModInitializer
             {
                 int x = screen.width / 2;
                 int y = screen.height / 4 + 48;
+                Screens.getButtons(screen).add(new CatalogueIconButton(x - 124, y + 48, 30, 0, button -> {
+                    client.setScreen(new CatalogueModListScreen(screen));
+                }, (button, poseStack, mouseX, mouseY) -> {
+                    screen.renderTooltip(poseStack, Component.translatable("catalogue.gui.mod_list"), mouseX, mouseY);
+                }));
+            }
+            else if(screen instanceof PauseScreen)
+            {
+                int x = screen.width / 2;
+                int y = screen.height / 4 + 32;
                 Screens.getButtons(screen).add(new CatalogueIconButton(x - 124, y + 48, 30, 0, button -> {
                     client.setScreen(new CatalogueModListScreen(screen));
                 }, (button, poseStack, mouseX, mouseY) -> {
